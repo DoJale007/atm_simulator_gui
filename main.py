48cb4a8e33fd0e873926609d8681cd48d25a946a
DoJale007 <israeljale98@gmail.com>
2025-08-20 12:52:18 +0000

    Creating an ATM GUI with Python

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import tkinter as tk
 from commands import check_balance
+from deposit import show_deposit_ui
 
 # Create the main window
 root = tk.Tk()
 
 #add a tittle to main window
 root.title("ATM Simulator")
-#Specify main window size
-root.geometry("500x400")
+
+# Create a tk frame for main window
+frame = tk.Frame(root, padx=50, pady=50 )
+frame.grid(row=0, column=0, sticky="nsew")
+
 
 #Add account number entry
-account_number_entry = tk.Entry(root, width=50)
-account_number_entry.pack(side="top")
+account_number_entry = tk.Entry(frame, width=50)
+account_number_entry.grid(column=1, row=1)
 
 #Add check balance button
-check_balance_btn = tk.Button(root, text="Check Balance", command=lambda: check_balance(account_number=account_number_entry.get()))
-check_balance_btn.pack(side="top")
+check_balance_btn = tk.Button(frame, text="Check Balance", command=lambda: check_balance(account_number=account_number_entry.get()))
+check_balance_btn.grid(column=2, row=1)
 
 #Add deposit button
-deposit_btn = tk.Button(root, text="Deposit")
-deposit_btn.pack(side="top")
+deposit_btn = tk.Button(frame, text="Deposit", command=lambda : show_deposit_ui(root))
+deposit_btn.grid(column=2, row=3)
+
+
 #Open the main window
 root.mainloop()
